c9c8b3e719a8569f3f32858aadc9da5c8ecf5995
MishkatMukit <mahabubmishkat8@gmail.com>
2026-07-06 03:21:08 +0600

    initial server setup completed

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,26 @@
-import express, { type Application } from "express"
+import express, { type Application, type Request, type Response } from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
+import config from "./config"
 
 
 const app : Application = express()
 
 app.use(cors({
-    origin: config.app
+    origin: config.app_url,
+    credentials: true
 }))
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+
+app.get('/', async (req: Request, res: Response) => {
+    res.json({
+        message : "GearUp server is running",
+        Auhtor : "Mishkat Mahabub"
+    })
+    
+})
+
+export default app
--- src/config/index.ts
+++ src/config/index.ts
@@ -4,5 +4,6 @@ import path from "path"
 dotenv.config({ path: path.join(process.cwd(), ".env") });
 
 export default {
-    port : process.env.PORT || 5000
+    port : process.env.PORT || 5000,
+    app_url: process.env.APP_URL,
 }
