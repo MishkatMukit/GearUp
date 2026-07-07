eee7a6c6ba8082fabf9aba9409ba54d041e050c2
MishkatMukit <mahabubmishkat8@gmail.com>
2026-07-07 15:06:21 +0600

    added get gear by id

--- src/modules/gear/gear.route.ts
+++ src/modules/gear/gear.route.ts
@@ -6,5 +6,6 @@ import { gearController } from "./gear.controller";
 const router  = Router()
 
 router.get('/',gearController.getAllGear )
+router.get('/:id', gearController.getGearById)
 
 export const gearRoutes = router
