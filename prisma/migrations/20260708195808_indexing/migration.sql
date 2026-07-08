9e01d63864597d80f7f944c11a35ca0e7311112b
MishkatMukit <mahabubmishkat8@gmail.com>
2026-07-09 02:10:33 +0600

    feat: enhance user registration validation and add database indexing

---
+++ prisma/migrations/20260708195808_indexing/migration.sql
@@ -0,0 +1,8 @@
+-- CreateIndex
+CREATE INDEX "gear_items_categoryId_idx" ON "gear_items"("categoryId");
+
+-- CreateIndex
+CREATE INDEX "gear_items_providerId_idx" ON "gear_items"("providerId");
+
+-- CreateIndex
+CREATE INDEX "payments_customerId_idx" ON "payments"("customerId");
--- src/modules/auth/auth.services.ts
+++ src/modules/auth/auth.services.ts
@@ -16,11 +16,12 @@ const registerUserInDB  = async(payload: RegisterUserPayload)=>{
     if (isUserExist) {
         throw new Error("User with this email already exists");
     }
-    // const safeRole = role === Role.PROVIDER ? Role.PROVIDER : Role.CUSTOMER
-    if(role===Role.ADMIN){
+
+    if (role === Role.ADMIN) {
         throw new Error("You are not eligible for ADMIN role");
     }
 
+    const safeRole = role === Role.PROVIDER ? Role.PROVIDER : Role.CUSTOMER
     const hashedPassword = await bcrypt.hash(password, Number(config.bcrypt_salt_rounds));
 
     const createdUser = await prisma.user.create({
